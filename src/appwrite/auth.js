50dd5f2df72b60818bcbe6f2888b82a0057df2bd
Shubbu03 <thatcoderguyshubham@gmail.com>
2023-12-12 10:49:50 +0530

    Add AuthService class for authentication

---
+++ src/appwrite/auth.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-useless-catch */
+import config from "../config/config.js";
+import { Client, ID, Account } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectID);
+
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+
+      if (userAccount) {
+        return this.login(email, password);
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      const value = await this.account.get();
+
+      if (value) {
+        return value;
+      }
+    } catch (error) {
+      throw error;
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+export default authService;
