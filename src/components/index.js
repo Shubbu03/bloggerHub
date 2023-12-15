9c32048c4821d1b918ef32ddf4ad0fa3cf91c932
Shubbu03 <thatcoderguyshubham@gmail.com>
2023-12-15 16:06:06 +0530

    Add AuthLayout, Login, PostCard, Select, and Signup components

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,20 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutButton from "./header/LogoutButton";
 import Select from "./Select";
+import PostCard from "./PostCard";
+import Login from "./Login";
+import AuthLayout from "./AuthLayout";
+import Signup from "./Signup";
 
-export { Header, Footer, Container, Logo, LogoutButton, Select };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutButton,
+  Select,
+  AuthLayout,
+  PostCard,
+  Login,
+  Signup,
+};
