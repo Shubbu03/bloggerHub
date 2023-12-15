707516310f87549519dc479de21128f097e8b917
Shubbu03 <thatcoderguyshubham@gmail.com>
2023-12-15 14:49:01 +0530

    Add new components and update Header component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutButton from "./header/LogoutButton";
+import Select from "./Select";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutButton, Select };
